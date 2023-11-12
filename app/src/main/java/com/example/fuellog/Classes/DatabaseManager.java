775eb77c529aa2626761a9f02ba6b23615ebca7f
Gabriel Vaz <gabriel.vzpaula@gmail.com>
2023-11-12 13:18:19 -0300

    Nono commit

--- app/src/main/java/com/example/fuellog/Classes/DatabaseManager.java
+++ app/src/main/java/com/example/fuellog/Classes/DatabaseManager.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.Date;
+
 public class DatabaseManager {
 
     private DataBase dbHelper;
@@ -32,10 +34,29 @@ public class DatabaseManager {
         return database.insert(nomeTabela, null, values);
     }
 
+    //
+
+    public long insertUsuario(String nomeUsu,String naciUso, String dtNascUsu,
+                              String sexoUsu, String cpfUsu, String emailUsu,
+                              String telUsu, int senhaUsu) {
+
+        ContentValues values = new ContentValues();
+        values.put("nomeUsu", nomeUsu);      // String
+        values.put("naciUso", naciUso);      // String
+        values.put("dtNascUsu", dtNascUsu);  // String (representando uma data)
+        values.put("sexoUsu", sexoUsu);      // String
+        values.put("cpfUsu", Long.parseLong(cpfUsu));        // Long
+        values.put("emailUsu", emailUsu);    // String
+        values.put("telUsu", Long.parseLong(telUsu));        // Long
+        values.put("senhaUsu", senhaUsu);    // Integer
+
+        return database.insert("Usuarios", null, values);
+    }
+
     // Consultar todos os dados da tabela
     public Cursor getAllData() {
-        String[] columns = {"id", "nome"};
-        return database.query("TabelaExemplo", columns, null, null, null, null, null);
+        String[] columns = {"id", "nomeUsu"};
+        return database.query("USUARIOS", columns, null, null, null, null, null);
     }
 
     // Atualizar dados na tabela
--- app/src/main/java/com/example/fuellog/MainActivity.java
+++ app/src/main/java/com/example/fuellog/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fuellog;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.example.fuellog.Classes.DatabaseManager;
@@ -38,7 +39,34 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         DatabaseManager dbManager = new DatabaseManager(this);
+        dbManager.open();
+        insertUsuario(dbManager);
+        lerUsuario(dbManager);
+        dbManager.close();
 
+    }
+
+    private void insertUsuario(DatabaseManager dbManager) {
+        long newRowId = dbManager.insertUsuario("Gabriel", "Brasileiro", "1999-09-01", "Masculino", "06309873199", "gabriel.vzpaula@gmail.com", "62992489754", 123456);
+    }
+
+    private void lerUsuario(DatabaseManager dbManager) {
+        // Chame o método getAllData e obtenha um Cursor
+        Cursor cursor = dbManager.getAllData();
+
+        // Faça algo com o Cursor, como exibir os dados em um ListView ou RecyclerView
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(cursor.getColumnIndex("id"));
+                String nome = cursor.getString(cursor.getColumnIndex("nomeUsu"));
+                // Faça algo com os dados
+            } while (cursor.moveToNext());
+        } else {
+
+        }
+
+        // Certifique-se de fechar o Cursor quando não precisar mais dele
+        cursor.close();
     }
 
     @Override
@@ -57,7 +85,11 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            DatabaseManager dbManager = new DatabaseManager(this);
+            dbManager.open();
+            insertUsuario(dbManager);
+            lerUsuario(dbManager);
+            dbManager.close();
         }
 
         return super.onOptionsItemSelected(item);
